0222dd5e20134f1bc6d86964ec06fd7702c8aa1a
Pingger Shikkoken <pingger_github_public@iskariot.info>
2020-12-01 16:04:08 +0100

    Idea for external API Implementation

---
+++ src/main/java/info/iskariot/pingger/java/bukkit/serverTools/api/APIImplementation.java
@@ -0,0 +1,12 @@
+package info.iskariot.pingger.java.bukkit.serverTools.api;
+
+import info.iskariot.pingger.java.bukkit.serverTools.Module;
+
+/**
+ * @author Pingger
+ * @version 0.0.1
+ */
+public abstract class APIImplementation extends Module
+{
+
+}
