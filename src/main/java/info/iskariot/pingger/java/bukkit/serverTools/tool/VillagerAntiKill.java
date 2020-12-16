f9e183275979808dc1b1ff8c9c3b93b430c1feb2
Pingger Shikkoken <pingger_github_public@iskariot.info>
2020-12-16 01:25:42 +0100

    Villager AntiKill now allows Killing, when enough Villagers are in the area

--- src/main/java/info/iskariot/pingger/java/bukkit/serverTools/tool/VillagerAntiKill.java
+++ src/main/java/info/iskariot/pingger/java/bukkit/serverTools/tool/VillagerAntiKill.java
@@ -1,11 +1,18 @@
 package info.iskariot.pingger.java.bukkit.serverTools.tool;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.UUID;
+
+import org.bukkit.Location;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityTargetEvent;
+import org.bukkit.plugin.RegisteredListener;
 
 import info.iskariot.pingger.java.bukkit.serverTools.Module;
 
@@ -16,11 +23,27 @@ import info.iskariot.pingger.java.bukkit.serverTools.Module;
  */
 public class VillagerAntiKill extends Module implements Listener
 {
+
+	private HashSet<UUID> resets = new HashSet<>();
+
 	@Override
 	public void loadConfigDefaults()
 	{
 		ensureConfig("enabled", true, null);
 		ensureConfig("logging", false, null);
+		ensureConfig("minVillagers", 4, "Required Amount of Villagers in the Area to allow damage");
+		ensureConfig("checkRadius", 24, "The radius to search for other villagers. Higher Values WILL affect performance");
+	}
+
+	@Override
+	public void onConfigReload()
+	{
+		super.onConfigReload();
+		List<String> uuids = getConfig().getStringList("resetVillagers");
+		for (String uuid : uuids) {
+			resets.add(UUID.fromString(uuid));
+		}
+		getConfig().set("resetVillagers", resets.stream().map(u -> u.toString()).toArray());
 	}
 
 	@Override
@@ -33,6 +56,10 @@ public class VillagerAntiKill extends Module implements Listener
 	public void onEnable()
 	{
 		stp.getServer().getPluginManager().registerEvents(this, stp);
+		List<String> uuids = getConfig().getStringList("resetVillagers");
+		for (String uuid : uuids) {
+			resets.add(UUID.fromString(uuid));
+		}
 	}
 
 	/**
@@ -46,13 +73,34 @@ public class VillagerAntiKill extends Module implements Listener
 	{
 		if (isEnabled()) {
 			if (ede.getEntityType() != null && ede.getEntityType().equals(EntityType.VILLAGER)) {
-
-				log(
-						() -> "Cancelling Damage to " + ede.getEntity().toString() + "; Cause: " + ede.getCause().toString() + "; Amount: "
-								+ ede.getDamage()
-				);
-				ede.setCancelled(true);
-				ede.setDamage(0);
+				Location l = ede.getEntity().getLocation();
+				double r = getConfig().getDouble("radius");
+				if (l.getWorld().getNearbyEntities(l, r, r, r, e -> e.getType().equals(EntityType.VILLAGER)).size() < getConfig()
+						.getInt("minVillagers"))
+				{
+					log(
+							() -> "Cancelling Damage to " + ede.getEntity().toString() + "; Cause: " + ede.getCause().toString() + "; Amount: "
+									+ ede.getDamage()
+					);
+					ede.setCancelled(true);
+					ede.setDamage(0);
+				}
+				if (isDebug()) {
+					for (RegisteredListener rl : EntityDamageEvent.getHandlerList().getRegisteredListeners()) {
+						debug(
+								() -> "Listener: " + rl.getPlugin().getClass().getCanonicalName() + " -> "
+										+ rl.getListener().getClass().getCanonicalName()
+						);
+					}
+				}
+				Villager v = (Villager) ede.getEntity();
+				if (v.getVillagerExperience() == 1) {
+					if (!resets.contains(v.getUniqueId())) {
+						v.setVillagerExperience(0);
+						resets.add(v.getUniqueId());
+						stp.reloadConfig();
+					}
+				}
 			}
 		}
 	}
